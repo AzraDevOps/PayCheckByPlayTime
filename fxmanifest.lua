bdbaa7bd0084708e1f4b265f215886b77c61e24b
Harry AzraDevOps <72076704+AzraDevOps@users.noreply.github.com>
2022-11-13 12:07:45 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,22 +1,12 @@
---[[
-    #####################################################################
-    #                _____           __          _                      #
-    #               |  __ \         / _|        | |                     #
-    #               | |__) | __ ___| |_ ___  ___| |__                   #
-    #               |  ___/ '__/ _ \  _/ _ \/ __| '_ \                  #
-    #               | |   | | |  __/ ||  __/ (__| | | |                 #
-    #               |_|   |_|  \___|_| \___|\___|_| |_|                 #
-    #                                                                   #
-    #             Prefech_PlayTime By Prefech 28-10-2021                #
-    #                         www.prefech.com                           #
-    #                                                                   #
-    #####################################################################
-]]
+--[[ Forked from prefech/Prefech_playTime (here : https://github.com/prefech/Prefech_playTime) ]]
+fx_version 'cerulean'
+game 'gta5'
 
+author 'Harry MEYERS#6408'
+contact 'https://discord.gg/UWgQUKA8uc'
 
-author 'Prefech'
-description 'Prefech_PlayTime'
-version '1.0.8'
+description 'PayCheckByPlayTime forked from prefech/Prefech_playTime'
+version '1.0.0'
 
 -- Server Scripts
 server_scripts {
@@ -29,5 +19,4 @@ client_scripts {
     'client.lua'
 }
 
-game 'gta5'
-fx_version 'cerulean'
+
